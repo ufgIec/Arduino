93fef2720ecd4b65c1caa21fd5869a1e7298bfed
Antonio <iecufg@gmail.com>
2020-01-28 14:25:49 -0300

    Alteração codigo do regador automatico

--- Monitoria 2019.2/Códigos/Projetos engemulti/Regador automatico/sketch_jan22a/sketch_jan22a.ino
+++ Monitoria 2019.2/Códigos/Projetos engemulti/Regador automatico/sketch_jan22a/sketch_jan22a.ino
@@ -5,7 +5,10 @@
 #define rele 13
 #define led1 6
 #define led2 7
+#include <LiquidCrystal.h>
  
+//Define os pinos que serão utilizados para ligação ao display
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 int valor_analogico;
  
 void setup()
@@ -15,6 +18,7 @@ void setup()
   pinMode(rele, OUTPUT);
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
+    lcd.begin(16, 2);
 }
  
 void loop()
@@ -31,8 +35,14 @@ void loop()
   {
     Serial.println(" Status: Solo umido");
     digitalWrite(rele, LOW);
-    digitalWrite(led1, HIGH);
+    digitalWrite(led1, LOW);
         digitalWrite(led2, LOW);
+          lcd.clear();
+  //Posiciona o cursor na coluna 3, linha 0;
+  lcd.setCursor(3, 0);
+  //Envia o texto entre aspas para o LCD
+  lcd.print("SOLO UMIDO!");
+delay(4000);
   }
  
   //Solo com umidade moderada, acende led amarelo
@@ -40,8 +50,14 @@ void loop()
   {
     Serial.println(" Status: Umidade moderada");
     digitalWrite(rele, LOW);
-    digitalWrite(led1, HIGH);
+    digitalWrite(led1, LOW);
         digitalWrite(led2, LOW);
+          lcd.clear();
+  //Posiciona o cursor na coluna 3, linha 0;
+  lcd.setCursor(3, 0);
+  //Envia o texto entre aspas para o LCD
+  lcd.print("SOLO OK!");
+  delay(4000);
     
   }
  
@@ -50,10 +66,27 @@ void loop()
   {
     Serial.println(" Status: Solo seco");
     digitalWrite(rele, HIGH);
+    digitalWrite(led1,HIGH);
+    delay(500);
     digitalWrite(led1,LOW);
     digitalWrite(led2, HIGH);
+      lcd.clear();
+  //Posiciona o cursor na coluna 3, linha 0;
+  lcd.setCursor(3, 0);
+  //Envia o texto entre aspas para o LCD
+  lcd.print("SOLO SECO!");
+  lcd.setCursor(3, 1);
+  lcd.print("REGANDO.....");
+  delay(4000);
 
   }
+  lcd.clear();
+  //Posiciona o cursor na coluna 3, linha 0;
+  lcd.setCursor(1, 0);
+  //Envia o texto entre aspas para o LCD
+  lcd.print("REGADOR AUTOM.");
+  lcd.setCursor(0, 1);
+  lcd.print(" UFG - ENG. COMP.");
   delay(3000);
 }
  
